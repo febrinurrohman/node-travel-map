84c70b1a46b2d80f76729d1b9fe102f5ed7c58db
Febri Nur Rohman <vanderfarrel@gmail.com>
2021-07-23 16:59:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("MongoDB Connected!");
+}).catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+});
